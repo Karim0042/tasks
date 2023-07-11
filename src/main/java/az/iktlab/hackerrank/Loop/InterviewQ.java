59533a5c47bd88c237d9ec0d04cdb6d299d48efd
Karim <2KarimKarimov004@gmail.com>
2023-07-11 14:21:43 +0400

    oop

--- src/main/java/az/iktlab/hackerrank/Loop/InterviewQ.java
+++ src/main/java/az/iktlab/hackerrank/Loop/InterviewQ.java
@@ -9,14 +9,20 @@ public class InterviewQ {
         //0,1,2  |  2,1,0  |  0,1,2  arr[1][0]
         //3,4,5  |  5,4,3  |  3,4,5
         Integer[] step = {0, 1, 2};
+        Integer[] nStep = {2, 1, 0};
+        Integer[] tmp = {0, 0, 0};
         for (int i = 0; i < 3; i++) {
             int temp = 0;
-            System.out.print(arr[i][step[temp] % 3]+" ");
+            System.out.print(arr[i][step[temp] % 3] + " ");
             temp++;
             System.out.print(arr[i][step[temp] % 3] + " ");//2,5
             temp++;
             System.out.print(arr[i][step[temp] % 3] + " ");//3,
-            Collections.reverse(Arrays.asList(step));
+            tmp = step;
+            step = nStep;
+            nStep = step;
+            //Collections.reverse(Arrays.asList(step));
+
         }
     }
 }
