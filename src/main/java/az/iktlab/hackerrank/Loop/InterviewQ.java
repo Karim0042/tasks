1642416836fcdf26c8ce0b5a696f6ebb9dd89567
Karim <2KarimKarimov004@gmail.com>
2023-07-11 14:25:13 +0400

    oop

--- src/main/java/az/iktlab/hackerrank/Loop/InterviewQ.java
+++ src/main/java/az/iktlab/hackerrank/Loop/InterviewQ.java
@@ -1,27 +1,24 @@
 package az.iktlab.hackerrank.Loop;
 
-import java.util.Arrays;
-import java.util.Collections;
-
 public class InterviewQ {
     public static void main(String[] args) {
         int[][] arr = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
         //0,1,2  |  2,1,0  |  0,1,2  arr[1][0]
         //3,4,5  |  5,4,3  |  3,4,5
-        Integer[] step = {0, 1, 2};
-        Integer[] nStep = {2, 1, 0};
-        Integer[] tmp = {0, 0, 0};
+        Integer[] index = {0, 1, 2};
+        Integer[] step = {2, 1, 0};
+        Integer[] tmp = new Integer[index.length];
         for (int i = 0; i < 3; i++) {
             int temp = 0;
-            System.out.print(arr[i][step[temp] % 3] + " ");
+            System.out.print(arr[i][index[temp] % 3] + " ");
             temp++;
-            System.out.print(arr[i][step[temp] % 3] + " ");//2,5
+            System.out.print(arr[i][index[temp] % 3] + " ");//2,5
             temp++;
-            System.out.print(arr[i][step[temp] % 3] + " ");//3,
-            tmp = step;
-            step = nStep;
-            nStep = step;
-            //Collections.reverse(Arrays.asList(step));
+            System.out.print(arr[i][index[temp] % 3] + " ");//3,
+            tmp = index;
+            index = step;
+            step = index;
+            //Collections.reverse(Arrays.asList(index));
 
         }
     }
