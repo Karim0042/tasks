550aee832994f064000dff0e78085e90fbe4d763
Karim <2KarimKarimov004@gmail.com>
2023-08-04 23:06:24 +0400

    oop

---
+++ src/main/java/az/iktlab/hackerrank/oop/animals/DomesticCat.java
@@ -0,0 +1,16 @@
+package az.iktlab.hackerrank.oop.animals;
+
+import az.iktlab.hackerrank.oop.Pet;
+import az.iktlab.hackerrank.oop.enums.Species;
+
+public class DomesticCat extends Pet  {
+    DomesticCat(){
+        super.setSpecies(Species.CAT);
+    }
+    @Override
+    public void respond() {
+
+    }
+
+
+}
